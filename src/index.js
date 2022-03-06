c303e6ca8f6205512df55f3ff0729fcb2f33fd3b
TheGBO <gabrielthejota@gmail.com>
2022-03-05 22:24:01 -0300

    trying to deploy

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const routes = require('./routes');
 
 const port = process.env.PORT || 3000
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
--- src/routes.js
+++ src/routes.js
@@ -13,7 +13,56 @@ router.get('/', async (req, res) => {
 
 router.post('/post', auth, async (req, res) => {
 	try {
-		res.json(suc(true,{},"Post Created"));
+		const postcreated = await pool.query('INSERT INTO posts(title, content, thumbnail) VALUES ($1,$2,$3) RETURNING *', [
+			req.body.title,
+			req.body.content,
+			req.body.thumbnail
+		]);
+		res.json(suc(true,postcreated.rows[0],"Post Created"));
+	} catch (error) {
+		res.json(suc(false,{},"Internal Server Error"));
+	}
+});
+
+router.get('/post', async (req, res) => {
+	try {
+		const postsRetreived = await pool.query('SELECT * FROM posts');
+		res.json(suc(true,postsRetreived.rows,"Posts Retreived"));
+	} catch (error) {
+		console.log(error.message);
+		res.json(suc(false,{},"Internal Server Error"));
+	}
+});
+
+router.get('/post/:id', async (req, res) => {
+	try {
+		const postsRetreived = await pool.query('SELECT * FROM posts WHERE id = $1', [req.params.id]);
+		res.json(suc(true,postsRetreived.rows[0],"Single Post Retreived"));
+	} catch (error) {
+		console.log(error.message);
+		res.json(suc(false,{},"Internal Server Error"));
+	}
+});
+
+router.delete('/post/:id', auth, async (req, res) => {
+	try {
+		const postsRetreived = await pool.query('DELETE FROM posts WHERE id = $1', [req.params.id]);
+		res.json(suc(true,{},"Post Deleted"));
+	} catch (error) {
+		console.log(error.message);
+		res.json(suc(false,{},"Internal Server Error"));
+	}
+});
+
+router.put('/post/:id', auth, async (req, res) => {
+	try {
+		const postcreated = await pool.query('UPDATE POSTS SET title=$1, content=$2, thumbnail=$3 WHERE id = $4 RETURNING *', [
+			req.body.title,
+			req.body.content,
+			req.body.thumbnail,
+			req.params.id
+		]);
+		res.json(suc(true,postcreated.rows[0],"Post Updated"));
 	} catch (error) {
 		res.json(suc(false,{},"Internal Server Error"));
 	}
